2c684b2b75eaa1ddf377a5c59042d922d607835a
Deepak Choudhary <deepakc35@gmail.com>
2026-05-12 19:30:12 +0530

    Update JS basics and add var/const/let examples

--- Chaptor_01_Basics/01_Basics.js
+++ Chaptor_01_Basics/01_Basics.js
@@ -1,1 +1,8 @@
 console.log("Hello World!");
+
+let name = "Deepak";
+let Name = "Deepak";
+var name = "Deepak";
+
+console.log(name);
+console.log(Name);
---
+++ Chaptor_01_Basics/06_var_const_let.js
@@ -0,0 +1,17 @@
+for (var i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 100);
+}
+
+
+for (let j = 0; j < 3; j++) {
+    setTimeout(() => console.log(j), 100);
+}
+
+for (var i = 0; i < 3; i++) {
+    console.log(i);
+}
+
+
+for (let j = 0; j < 3; j++) {
+    console.log(j);
+}
